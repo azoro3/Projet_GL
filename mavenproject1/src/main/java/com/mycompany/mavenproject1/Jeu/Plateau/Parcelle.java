05691d10f6e09553a93b5d68669227ee04ccbf46
aZoro3 <arthur.rozo@outlook.fr>
2017-03-21 16:20:00 +0100

    no message

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Jeu/Plateau/Parcelle.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1.Jeu.Plateau;
+
+import com.mycompany.mavenproject1.Jeu.Tuiles;
+
+/**
+ *
+ * @author Arthur
+ */
+public class Parcelle {
+    private Tuiles [] ensTuiles= new Tuiles[4];
+    
+}
