fb7e36013235f6b629c48c835ec6b955f4d1433f
aZoro3 <arthur.rozo@outlook.fr>
2017-03-21 15:26:24 +0100

    Création des première classe

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Jeu/Canal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1.Jeu;
+
+/**
+ *
+ * @author Arthur
+ */
+public class Canal {
+    private int xDeb;
+    private int yDeb;
+    private int xFin;
+    private int yFin;
+    
+}
---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Jeu/Joueur.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1.Jeu;
+
+/**
+ *
+ * @author Arthur
+ */
+public class Joueur {
+    private String nom;
+    private String couleur;
+    private Canal canal;
+    private int solde;
+    private int travailleurs;
+    private boolean estConstructeur = false;
+    
+}
