ca0e08f7acf9c210910b6c583cf2491e92c5fafc
Magalie Mallinger <magalie.07@live.fr>
2017-04-10 17:49:58 +0200

    Branche interface_v2 : Affichage des premières tuiles

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/MainApp.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/MainApp.java
@@ -33,8 +33,8 @@ public class MainApp extends Application {
      */
     public static void main(String[] args) {
         launch(args);
-        Source source = Source.getInstance();
-        source.toString();
+        //Source source = Source.getInstance();
+        //source.toString();
         Partie partie = new Partie();
     }
 
--- mavenproject1/src/main/java/com/mycompany/mavenproject1/PlateauController.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/PlateauController.java
@@ -1,6 +1,7 @@
 package com.mycompany.mavenproject1;
 
 import com.mycompany.mavenproject1.Jeu.Plateau.Source;
+import com.mycompany.mavenproject1.Jeu.Tuiles;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -13,7 +14,7 @@ public class PlateauController implements Initializable {
 
     @FXML
     private GridPane plateau;
-    
+
     @FXML
     private ImageView tuile1;
 
@@ -23,10 +24,67 @@ public class PlateauController implements Initializable {
         final ImageView img = new ImageView();
         final Image sourceImg = new Image(getClass().getResourceAsStream("/images/source.png"));
         img.setImage(sourceImg);
-        
+
         // Placement aléatoire de la source
         plateau.add(img, Source.getInstance().getX(), Source.getInstance().getY());
-        
-        
+    }
+
+    public void initPartie() {
+        // Initialisation d'une nouvelle partie
+        Partie partie = new Partie();
+        partie.initPartie();
+
+        Image image;
+
+        // Première tuile de chaque pile
+        Tuiles[] tuiles = partie.getFirstCarte();
+
+        // Test avec la première tuile
+        String type = tuiles[0].getType();
+        int nbTravailleurs = tuiles[0].getNbTravailleurs();
+
+        switch (type) {
+            case ("piment"):
+                if (nbTravailleurs == 1) {
+                    image = new Image(getClass().getResourceAsStream("/images/piment1.jpg"));
+                } else {
+                    image = new Image(getClass().getResourceAsStream("/images/piment2.jpg"));
+                }
+                tuile1.setImage(image);
+                break;
+            case ("haricot"):
+                if (nbTravailleurs == 1) {
+                    image = new Image(getClass().getResourceAsStream("/images/haricot1.jpg"));
+                } else {
+                    image = new Image(getClass().getResourceAsStream("/images/haricot2.jpg"));
+                }
+                tuile1.setImage(image);
+                break;
+            case ("banane"):
+                if (nbTravailleurs == 1) {
+                    image = new Image(getClass().getResourceAsStream("/images/banane1.jpg"));
+                } else {
+                    image = new Image(getClass().getResourceAsStream("/images/banane2.jpg"));
+                }
+                tuile1.setImage(image);
+                break;
+            case ("patate"):
+                if (nbTravailleurs == 1) {
+                    image = new Image(getClass().getResourceAsStream("/images/patate1.jpg"));
+                } else {
+                    image = new Image(getClass().getResourceAsStream("/images/patate2.jpg"));
+                }
+                tuile1.setImage(image);
+                break;
+            case ("sucre"):
+                if (nbTravailleurs == 1) {
+                    image = new Image(getClass().getResourceAsStream("/images/sucre1.jpg"));
+                } else {
+                    image = new Image(getClass().getResourceAsStream("/images/sucre2.jpg"));
+                }
+                tuile1.setImage(image);
+                break;
+        }
+
     }
 }
